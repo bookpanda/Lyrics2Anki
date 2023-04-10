3412c62b402e38a4dbd251389e8509532c792345
Idhibhat Pankam <marginpankam@gmail.com>
2023-04-10 23:05:10 +0700

    feat(server): tokenizer complete

--- apps/server/main.py
+++ apps/server/main.py
@@ -1,16 +1,34 @@
 from flask import Flask, request, Response
 from flask_cors import CORS, cross_origin
 from threading import Thread
+import json
+from fugashi import Tagger
 
 app = Flask(__name__)
 cors = CORS(app)
+
+fukashitTagger = Tagger()
 
 
 @cross_origin()
 @app.route("/", methods=["POST"])
 def index():
-    print("req")
-    content = request.form.get("contents")
+    lyrics = request.json
+    tokens = set()
+    for line in lyrics:
+        for word in fukashitTagger(line):
+            if word.feature[0] != "助詞":
+                tokens.add(word.surface)
+
+    token_list = [tk for tk in tokens]
+
+    data = {"content": token_list}
+    json_dump = json.dumps(data)
+    res = Response(json_dump)
+    res.headers["Content-Type"] = "application/json"
+    res.headers["Access-Control-Allow-Origin"] = "*"
+    res.headers["Access-Control-Allow-Credentials"] = True
+    return res
 
 
 def run():
