43ae5e22560917222bdde283d11e427fe3c45f3a
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-25 23:02:10 +0700

    fix: eslint import error

--- apps/client/src/components/home/ProblemModal/ProblemModal.tsx
+++ apps/client/src/components/home/ProblemModal/ProblemModal.tsx
@@ -10,46 +10,47 @@ export const ProblemModal = () => {
         useOpenContext();
     const { selectedSong } = useAppContext();
     return (
-        isProblemModalOpen &&
-        selectedSong && (
-            <div className="no-scrollbar hidden h-full w-1/4 overflow-auto rounded-xl bg-gray-600 p-4 xl:block">
-                <div className="flex h-8 items-center justify-between">
-                    <Text variant="h5" className="text-white">
-                        {selectedSong.album}
-                    </Text>
-                    <X
-                        className="h-8 w-8 rounded-full p-1 text-gray-textlight duration-300 hover:scale-110 hover:cursor-pointer hover:bg-gray-hl hover:text-white"
-                        onClick={closeProblemModal}
-                    />
-                </div>
-                <RoundDiv className="mt-4">
-                    <Image
-                        src={selectedSong.albumArt}
-                        alt="problem"
-                        style={{ width: "100%", height: "100%" }}
-                        className="rounded-md"
-                        unoptimized
-                    />
-                </RoundDiv>
-                <div className="mt-4 flex justify-between">
-                    <div>
-                        <Text variant="h3" className="text-white">
-                            <b>{selectedSong.title}</b>
+        <>
+            {isProblemModalOpen && selectedSong && (
+                <div className="no-scrollbar hidden h-full w-1/4 overflow-auto rounded-xl bg-gray-600 p-4 xl:block">
+                    <div className="flex h-8 items-center justify-between">
+                        <Text variant="h5" className="text-white">
+                            {selectedSong.album}
                         </Text>
-                        <Text variant="p1" className="text-gray-text">
-                            {selectedSong.artists}
-                        </Text>
+                        <X
+                            className="h-8 w-8 rounded-full p-1 text-gray-textlight duration-300 hover:scale-110 hover:cursor-pointer hover:bg-gray-hl hover:text-white"
+                            onClick={closeProblemModal}
+                        />
                     </div>
-                    <div className="flex w-20 items-center justify-center space-x-5">
-                        <div className="w-1/2">
-                            <MoreHorizontal
-                                onClick={openEditModal}
-                                className="hover:cursor-pointer' h-[50%] text-white hover:scale-110 hover:cursor-pointer"
-                            />
+                    <RoundDiv className="mt-4">
+                        <Image
+                            src={selectedSong.albumArt}
+                            alt="problem"
+                            style={{ width: "100%", height: "100%" }}
+                            className="rounded-md"
+                            unoptimized
+                        />
+                    </RoundDiv>
+                    <div className="mt-4 flex justify-between">
+                        <div>
+                            <Text variant="h3" className="text-white">
+                                <b>{selectedSong.title}</b>
+                            </Text>
+                            <Text variant="p1" className="text-gray-text">
+                                {selectedSong.artists}
+                            </Text>
+                        </div>
+                        <div className="flex w-20 items-center justify-center space-x-5">
+                            <div className="w-1/2">
+                                <MoreHorizontal
+                                    onClick={openEditModal}
+                                    className="hover:cursor-pointer' h-[50%] text-white hover:scale-110 hover:cursor-pointer"
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        )
+            )}
+        </>
     );
 };
