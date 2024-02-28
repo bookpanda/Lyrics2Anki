cc08a4d21cb9c1830ff20623ea86b8224f4fda58
Idhibhat Pankam <marginpankam@gmail.com>
2024-02-28 17:50:54 +0700

    feat: tf lambda

--- Makefile
+++ Makefile
@@ -4,8 +4,11 @@ pip-anki:
 package-anki:
 	cd apps/serverless/anki/package && zip -r ../package.zip . && cd .. && zip package.zip make_anki.py && mv package.zip ../
 
+zip-anki:
+	make pip-anki && make package-anki
+
 check-tf:
 	cd apps/serverless/anki && terraform fmt -check=true && terraform validate
 
 deploy-tf:
-	make check-tf && cd apps/serverless/anki && terraform init && terraform apply
+	make check-tf && cd apps/serverless && terraform init && terraform apply
--- apps/serverless/lambda.tf
+++ apps/serverless/lambda.tf
@@ -13,11 +13,48 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-07a6e3b1c102cdba8"
-  instance_type = "t2.micro"
+resource "aws_lambda_function" "l2a-make-anki" {
+  function_name = "l2a-make-anki"
+  handler       = "make_anki.lambda_handler"
+  runtime       = "python3.9"
+  filename      = "package.zip"
+  role          = aws_iam_role.lambda_exec.arn
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+  source_code_hash = filebase64sha256("package.zip")
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "l2a-make-anki"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "lambda.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name = "invoke-lambda"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "lambda:InvokeFunction",
+        Resource = aws_lambda_function.l2a-make-anki.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attachment" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
